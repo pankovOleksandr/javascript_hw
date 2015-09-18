7aec37406fd9dd0da4b44613f42cad491c8ebba7
PankovAleksander <pankov.oleksandr@gmail.com>
2015-09-18 13:18:33 +0300

    Lesson 1 complete

---
+++ lesson1/js/1.js
@@ -0,0 +1,15 @@
+"use strict"
+
+var developer="",
+    name="Sasha",
+    myName="";
+
+    developer = name;
+    alert(developer);
+
+myName = prompt("What is your name?", "Boris");
+alert('Your name is ' + myName);
+
+
+
+
