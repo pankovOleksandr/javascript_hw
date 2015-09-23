6203487ad3bbab9c54afeb2b27df9e6f51e11c37
PankovAleksander <pankov.oleksandr@gmail.com>
2015-09-23 14:48:55 +0300

    no message

---
+++ lesson2/js/1.js
@@ -0,0 +1,10 @@
+function year() {
+	var year = +prompt('Какой сейчас год?');
+
+	if (year==2015) {
+	  alert('You are right');
+	} else {
+	  alert('Have you fallen from the Moon? It is 2015');
+	};
+
+}
